99d7421d1ead9effad514c8b4d644ba74dd8e339
imsupeer <58670605+imsupeer@users.noreply.github.com>
2022-04-06 08:33:02 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,8 +4,8 @@
 #fábrica de um carro, calcular e escrever o custo final ao consumidor.
 
 custo_de_fabrica = float(input("Insira o valor do custo de fabricação: "))
-porcetagem_distribuidor = custo_de_fabrica * 0.28
-imposto= custo_de_fabrica * 0.45
-carro_novo = custo_de_fabrica + porcetagem_distribuidor +imposto
+porcetagem_distribuidor = custo_de_fabrica * 28/100
+imposto= custo_de_fabrica * 45/100
+carro_novo = custo_de_fabrica + porcetagem_distribuidor + imposto
 
 print(f"O valor do carro novo é: R${carro_novo}")
